3844032879fca58c241298df202ff3bececa93a3
rahulrameshm0 <iamrahulm23@gmail.com>
2025-01-29 00:26:08 +0530

    created function for  Rock paper and scissors

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,40 +1,48 @@
 import random
 emojis = {'r': '🪨', 'p': '📃', 's': '✃'}
 lists = ["r", "p", "s"]
-computer = 0
-user = 0
 
-while True:
-    user_input = input("rock, paper or scissors (r/p/s): ")
-    computer_choice = random.choice(lists)
-    if user_input not in lists:
-        print("Enter a valid input!")
-        continue
+def get_user_choice():
+    while True:
+        user_input = input("rock, paper or scissors (r/p/s): ")
+        if user_input in lists:
+            return user_input
+        else:
+            print("Enter a valid input!")
+            continue
+
+def display_choices(user_input, computer_choice):
     print(f"You chose: {emojis[user_input]}")
     print(f"Computer Chose: {emojis[computer_choice]}")
+
+def determine_winner(user_input, computer_choice):
     if (
-            (user_input == "r" and computer_choice == "s") or
-            (user_input == "s" and computer_choice == "p") or
-            (user_input == "p" and computer_choice == "r")):
+        (user_input == "r" and computer_choice == "s") or
+        (user_input == "s" and computer_choice == "p") or
+        (user_input == "p" and computer_choice == "r")):
         print("You win")
-        user += 1
-    elif(
-            (user_input == "s" and computer_choice == "s") or
-            (user_input == "p" and computer_choice == "p") or
-            (user_input == "r" and computer_choice == "r")):
+
+    elif (
+        (user_input == "s" and computer_choice == "s") or
+        (user_input == "p" and computer_choice == "p") or
+        (user_input == "r" and computer_choice == "r")):
         print("It's a draw!")
     else:
         print("You lose")
-        computer += 1
-    print(f"Your point = {user}")
-    print(f"Computer point = {computer}")
-    should_continue = input("Do you need to continue? (y/n): ").lower()
-    if should_continue == "y":
-        continue
-    elif should_continue == "n":
-        print("Thank you for playing")
-        break
-    else:
-        print("Enter a valid input!")
+def play_game():
+    while True:
+        user_input = get_user_choice()
+        computer_choice = random.choice(lists)
+        display_choices(user_input, computer_choice)
+        determine_winner(user_input, computer_choice)
 
+        should_continue = input("Do you need to continue? (y/n): ").lower()
+        if should_continue == "y":
+            continue
+        elif should_continue == "n":
+            print("Thank you for playing")
+            break
+        else:
+            print("Enter a valid input!")
 
+play_game()
