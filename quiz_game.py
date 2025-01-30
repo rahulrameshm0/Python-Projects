4102fa82d2bf1dfc102b8ece744cfcab6d1ef8a9
rahulrameshm0 <iamrahulm23@gmail.com>
2025-01-30 14:57:29 +0530

    created quiz game

---
+++ quiz_game.py
@@ -0,0 +1,34 @@
+
+question = [
+    {"Prompt": "What is the capital of India?",
+    "option": ["A.Rajasthan", "B.Delhi", "C.Gujarat", "D.Punjab"],
+    "answer":"B"},
+
+    {"Prompt": "What is the capital of Australia??",
+    "option": ["A.Sydney", "B.Canberra", "C.Melbourne", "D.Brisbane"],
+    "answer":"B"},
+
+    {"Prompt": "Which planet is known as the Red Planet?",
+    "option": ["A.Venus", "B.Mars", "C.Jupiter", "D.Mercury"],
+    "answer": "B"},
+
+    {"Prompt": "Who painted the Mona Lisa?",
+    "option": ["A.Vincent van Goghn", "B.Michelangelo", "C.Leonardo da Vinci", "D.Pablo Picasso"],
+    "answer": "C"}
+    ]
+
+def run_quiz(question):
+    count = 0
+    for i in question:
+        print(f"{i['Prompt']}")
+        for option in i['option']:
+            print(option)
+        option = input("Enter your option: ")
+
+        if option == i['answer']:
+            count += 1
+        else:
+            print("Wrong answer!")
+    print(f"You got {count} out of {len(i)}")
+
+run_quiz(question)
